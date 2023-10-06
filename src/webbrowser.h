51b39ac9e72aaeb13d638f6557aceb170fd57eb3
Steffen <steffen.diehl@rdf.nuernberg.de>
2023-10-06 17:13:31 +0200

    1.04

--- src/webbrowser.h
+++ src/webbrowser.h
@@ -20,37 +20,20 @@ IPAddress gateway(192, 168, 178, 1);    // Das Gateway
 IPAddress subnet(255, 255, 255, 0);   // Die Subnetzmaske
 IPAddress dns(8, 8, 8, 8);
 
-const char* PARAM_festerTimerName6 = "festerTimerName6";
-const char* PARAM_festerTimer6 = "festerTimer6";
-const char* PARAM_festerTimerName7 = "festerTimerName7";
-const char* PARAM_festerTimer7 = "festerTimer7";
-const char* PARAM_festerTimerName8 = "festerTimerName8";
-const char* PARAM_festerTimer8 = "festerTimer8";
-const char* PARAM_festerTimerName9 = "festerTimerName9";
-const char* PARAM_festerTimer9 = "festerTimer9";
-const char* PARAM_festerTimerName10 = "festerTimerName10";
-const char* PARAM_festerTimer10 = "festerTimer10";
-
-const char* PARAM_festeTimerNameSelect = "festeTimerNameSelect";
-const char* PARAM_timerSelect = "timerSelect";
-
-const char* PARAM_timer1value = "timer1value";
-const char* PARAM_timer2value = "timer2value";
-const char* PARAM_timer3value = "timer3value";
-const char* PARAM_timer4value = "timer4value";
-const char* PARAM_timer5value = "timer5value";
-
-
-String feste_Timer_Name[10] = {"Nudeln 7min", "Kartoffeln 14min", "Steak medium", "Mittag 1h", "Mittag 30min", "-", "-", "-", "-", "-"};
-unsigned long feste_Timer[10] = {420000, 840000, 90000, 3600000, 1800000, 0, 0, 0, 0, 0};
+const char* start_now_timer = "start_now_timer";
+int now_timer = 0;
+const char* left_now_timer = "left_now_timer";
+int left_timer = 0;
+const char* start_timer1 = "start_timer1";
+int start1 = 0;
+const char* end_timer1 = "end_timer1";
+int end1 = 0;
+const char* start_timer2 = "start_timer2";
+int start2 = 0;
+const char* end_timer2 = "end_timer2";
+int end2 = 0;
 
 const char* PARAM_CURRENTTIME = "currentTime";
-unsigned long timer_1 = 0;
-unsigned long timer_2 = 0;
-unsigned long timer_3 = 0;
-unsigned long timer_4 = 0;
-unsigned long timer_5 = 0;
-unsigned long timer[5];
 
 String Date = "";
 const char* ptr_Date = Date.c_str();
@@ -65,12 +48,10 @@ int *web_min = nullptr;
 int *web_sec = nullptr;
 
 int *web_status = nullptr;
-String web_status_color[3] = {"green", "red", "yellow"};
+String web_status_color[3] = {"DarkGreen", "DarkOrange", "FireBrick"};
 
 unsigned long _timer[5] = {0, 0, 0, 0, 0};
-// <center><font size='+2' color='blue'>Cisco Packet Tracer</font></center>
-// <html><body style="background-color: lightgreen;">
-// <hr>Welcome to Cisco Packet Tracer. Opening doors to new opportunities. Mind Wide Open
+
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML>
 <html><body style="background-color: lightgreen;">
@@ -101,29 +82,41 @@ const char index_html[] PROGMEM = R"rawliteral(
     <title>Maehrkel</title>
     <link rel="icon" type="image/jpg" href="/images/Merkel.jpg">
   </head>
+  <style>
+  /* Define the CSS style for the colored square */
+  .colored-square {
+    width: 100px; /* Adjust the width as needed */
+    height: 100px; /* Adjust the height as needed */
+    background-color: %statuscolor%; /* Change the color using a hexadecimal code */
+  }
+  </style>
   <body>
     <form target= "hidden-form">
       <h1>Maehrkel<small><small><small> by Daniel Becher, Benedikt Buettner, Steffen Diehl </small></small></small></h1>
       <h2>Date: %currentDate%</h2>
-      <h2>Current Time: %currentTime%</h2>
+      <h2>Time: %currentTime%</h2>
   <p><img src="https://www.bundeskanzler.de/resource/image/1860510/16x9/1023/575/1f8092e18cf8a8a122753b918d3e7016/aH/bundeskanzlerin-angela-merkel-portraet-2.jpg" alt="Smiley face" style="float:right;width:800px;height:450px;"></p>
     </form>
     <br>
     <form action="/stopnow" target="hidden-form">
       <input type="submit" style = background-color:FireBrick value="Stop now" onclick="stopnow()">
     </form>
-    <form action="/gohomw" target="hidden-form">
-      <input type="submit" style = background-color:DarkGreen value="Go home" onclick="gohome()">
+    <form action="/gohome" target="hidden-form">
+      <input type="submit" style = background-color:DarkOrange value="Go home" onclick="gohome()">
     </form>
     <br>
     <br>
     <form target= "hidden-form">
-      Timer: %timer1value% seconds / %timer1left% seconds left
+      Timer: %left_now_timer% seconds / %StartNow% seconds left
     </form>
     <form action="/startnow" target="hidden-form">
-      <input type="submit" style = background-color:DarkOrange value="Start now" onclick="startnow()">
+      <input type="submit" style = background-color:DarkGreen value="Start now" onclick="startnow()">
     </form>
     <br>
+    <body>
+      <!-- Create the colored square div element -->
+      <div class="colored-square"></div>
+    </body>
     <br>
     <form target= "hidden-form">
       <strong>Timer 1</strong>
@@ -150,42 +143,40 @@ const char index_html[] PROGMEM = R"rawliteral(
       Time till end: %timer4left% seconds
     </form>
     <br>
-
     <form action="/get" target="hidden-form">
-      Start now Timer (current value %festerTimerName6%): <input type="text" name="festerTimerName6">
+      Start now Timer (current value %StartNow%): <input type="text" name="start_now_timer">
       <br>
       <br>
-      Start Time 1 (current value %festerTimerName7%): <input type="text" name="festerTimerName7">
-      End Time 1 (current value %festerTimer7% ms): <input type="number " name="festerTimer7">
+      Start Time 1 (current value %Start1%): <input type="text" name="start_timer1">
+      End Time 1 (current value %End1% ms): <input type="number " name="end_timer1">
       <br>
-      Start Time 2 (current value %festerTimerName8%): <input type="text" name="festerTimerName8">
-      End Time 2 (current value %festerTimer8% ms): <input type="number " name="festerTimer8">
+      Start Time 2 (current value %Start2%): <input type="text" name="start_timer2">
+      End Time 2 (current value %End2% ms): <input type="number " name="end_timer2">
       <br>
       <input type="submit" value="Submit" onclick="submitMessage()">
     </form>
     <br>
     <br>
-
-  <form action="/start" target="hidden-form">
-    Timer %currentTimer%:
-    <label>festeTimerName:</label>
-    <select name="festeTimerNameSelect">
-    <option value="%festerTimer1%">%festerTimerName1%</option>
-    <option value="%festerTimer2%">%festerTimerName2%</option>
-    <option value="%festerTimer3%">%festerTimerName3%</option>
-    <option value="%festerTimer4%">%festerTimerName4%</option>
-    <option value="%festerTimer5%">%festerTimerName5%</option>
-    <option value="%festerTimer6%">%festerTimerName6%</option>
-    <option value="%festerTimer7%">%festerTimerName7%</option>
-    <option value="%festerTimer8%">%festerTimerName8%</option>
-    <option value="%festerTimer9%">%festerTimerName9%</option>
-    <option value="%festerTimer10%">%festerTimerName10%</option>
-    </select>
-    <input type="submit" value="Start a new Timer" onclick="startMessage()">
-  </form>
   <iframe style="display:none" name="hidden-form"></iframe>
   </body>
 </html>)rawliteral";
+  // <form action="/start" target="hidden-form">
+  //   Timer %currentTimer%:
+  //   <label>festeTimerName:</label>
+  //   <select name="festeTimerNameSelect">
+  //   <option value="%festerTimer1%">%festerTimerName1%</option>
+  //   <option value="%festerTimer2%">%festerTimerName2%</option>
+  //   <option value="%festerTimer3%">%festerTimerName3%</option>
+  //   <option value="%festerTimer4%">%festerTimerName4%</option>
+  //   <option value="%festerTimer5%">%festerTimerName5%</option>
+  //   <option value="%festerTimer6%">%festerTimerName6%</option>
+  //   <option value="%festerTimer7%">%festerTimerName7%</option>
+  //   <option value="%festerTimer8%">%festerTimerName8%</option>
+  //   <option value="%festerTimer9%">%festerTimerName9%</option>
+  //   <option value="%festerTimer10%">%festerTimerName10%</option>
+  //   </select>
+  //   <input type="submit" value="Start a new Timer" onclick="startMessage()">
+  // </form>
 
 // Replaces placeholder with stored values
 void notFound(AsyncWebServerRequest *request) {
@@ -194,14 +185,20 @@ void notFound(AsyncWebServerRequest *request) {
 
 // Replaces placeholder with stored values
 String processor(const String& var){
-  if(var == "festerTimerName10"){
-    return feste_Timer_Name[9];
+  if(var == "StartNow"){
+    return String(now_timer);
   }
-  else if(var == "festerTimer10"){
-    return String(feste_Timer[9]);
+  else if(var == "Start1"){
+    return String(start1);
   }
-  else if(var == "timer1value"){
-    return String(timer_1/1000);
+  else if(var == "End1"){
+    return String(end1);
+  }
+  else if(var == "Start2"){
+    return String(start2);
+  }
+  else if(var == "End2"){
+    return String(end2);
   }
   else if(var == "currentTime"){
     return Time;
@@ -209,8 +206,8 @@ String processor(const String& var){
   else if(var == "currentDate"){
     return Date;
   }
-  else if(var== "timer1left") {
-      return String(timer[0]/1000);
+  else if(var== "left_now_timer") {
+      return String(left_timer);
   }
   else if(var== "currentTimer") {
       return String(1);
@@ -258,7 +255,7 @@ void setup_webbrwoser(int *c_hour, int *c_min, int *c_sec, int *c_day, int *c_mo
   });
 
   server.on("/gohome", HTTP_GET, [](AsyncWebServerRequest *request){
-    *web_status = 3;
+    *web_status = 1;
   });
 
   server.on("/stopnow", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -266,7 +263,7 @@ void setup_webbrwoser(int *c_hour, int *c_min, int *c_sec, int *c_day, int *c_mo
   });
 
   server.on("/startnow", HTTP_GET, [](AsyncWebServerRequest *request){
-    *web_status = 1;
+    *web_status = 0;
   });
 
   server.on("/Date", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -276,24 +273,46 @@ void setup_webbrwoser(int *c_hour, int *c_min, int *c_sec, int *c_day, int *c_mo
   server.on("/Time", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", ptr_Time);
   });
+  server.on("/Status", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", (String(*web_status)).c_str());
+  });
 
-  // Send a GET request to <ESP_IP>/get?festerTimerName=<inputMessage>
   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
     unsigned long changetimer;
     String inputMessage;
-    // GET festerTimerName value on <ESP_IP>/get?festerTimerName=<inputMessage>
-    if (request->hasParam(PARAM_festerTimerName6)) {
-      inputMessage = request->getParam(PARAM_festerTimerName6)->value();
-      if(inputMessage != ""){
-        feste_Timer_Name[5] = inputMessage;
-      }
-    }
-    // GET inputInt value on <ESP_IP>/get?inputInt=<inputMessage>
-    if (request->hasParam(PARAM_festerTimer9)) {
-      inputMessage = request->getParam(PARAM_festerTimer9)->value();
+    if (request->hasParam(start_now_timer)) {
+      inputMessage = request->getParam(start_now_timer)->value();
       if(inputMessage != ""){
         changetimer = (inputMessage.toInt())*1000;
-        feste_Timer[8] = changetimer;
+        now_timer = changetimer;
+      }
+    }
+    if (request->hasParam(start_timer1)) {
+      inputMessage = request->getParam(start_timer1)->value();
+      if(inputMessage != ""){
+        changetimer = (inputMessage.toInt())*1000;
+        start1 = changetimer;
+      }
+    }
+    if (request->hasParam(end_timer1)) {
+      inputMessage = request->getParam(end_timer1)->value();
+      if(inputMessage != ""){
+        changetimer = (inputMessage.toInt())*1000;
+        end1 = changetimer;
+      }
+    }
+    if (request->hasParam(start_timer2)) {
+      inputMessage = request->getParam(start_timer2)->value();
+      if(inputMessage != ""){
+        changetimer = (inputMessage.toInt())*1000;
+        start2 = changetimer;
+      }
+    }
+    if (request->hasParam(end_timer2)) {
+      inputMessage = request->getParam(end_timer2)->value();
+      if(inputMessage != ""){
+        changetimer = (inputMessage.toInt())*1000;
+        end2 = changetimer;
       }
     }
     request->send(200, "text/text", inputMessage);
